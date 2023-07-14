0dc89b6a106cffaa3c44b2377020865941422e86
PixelFireXY <matteo.vittori@gmail.com>
2023-07-14 11:42:15 +0200

    Created world states.

---
+++ Assets/Scripts/WorldKeys/IsTired.cs
@@ -0,0 +1,11 @@
+using CrashKonijn.Goap.Behaviours;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MpcDailyRoutines
+{
+	public class IsTired : WorldKeyBase
+	{
+	} 
+}
